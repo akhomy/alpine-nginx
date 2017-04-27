924a994480a2888e4a7dd2f31ff751cb39467c92
Andriy Khomych <andrii.khomych@lemberg.co.uk>
2017-04-27 18:27:40 +0300

    Added new variables for manipulating timeouts in nginx.

--- README.md
+++ README.md
@@ -3,10 +3,15 @@ Nginx 1.10 image based on Alpine linux.
 
 ### Available options
 KEEPALIVE_TIMEOUT - keepalive_timeout param in nginx conf
+FASTCGI_READ_TIMEOUT - fastcgi_read_timeout param in nginx conf
 LISTEN_PORT  - port in which nginx will listen.
 SERVER_ROOT - container root directory
 SERVER_NAME - server name nginx param
 CLIENT_MAX_BODY_SIZE - client_max_body_size param in nginx conf
+PROXY_CONNECT_TIMEOUT - proxy_connect_timeout param in nginx conf
+PROXY_SEND_TIMEOUT - proxy_send_timeout param in nginx conf
+PROXY_READ_TIMEOUT - proxy_read_timeout param in nginx conf
+SEND_TIMEOUT - send_timeout param in nginx conf
 PROXY_PASS - proxy-container for processing php requests
 
 ### How to run
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ if [ -n "$KEEPALIVE_TIMEOUT" ]; then
       sed -i 's@^    keepalive_timeout.*@'"    keepalive_timeout           ${KEEPALIVE_TIMEOUT};"'@' /etc/nginx/nginx.conf
 fi
 
+if [ -n "$FASTCGI_READ_TIMEOUT" ]; then
+      sed -i 's@^    fastcgi_read_timeout.*@'"    fastcgi_read_timeout           ${FASTCGI_READ_TIMEOUT};"'@' /etc/nginx/nginx.conf
+fi
+
 if [ -n "$LISTEN_PORT" ]; then
       sed -i 's@^        listen.*@'"        listen                  ${LISTEN_PORT};"'@' /etc/nginx/nginx.conf
 fi
@@ -21,6 +25,23 @@ fi
 if [ -n "$CLIENT_MAX_BODY_SIZE" ]; then
       sed -i 's@^        client_max_body_size.*@'"        client_max_body_size                  ${CLIENT_MAX_BODY_SIZE};"'@' /etc/nginx/nginx.conf
 fi
+
+if [ -n "$PROXY_CONNECT_TIMEOUT" ]; then
+      sed -i 's@^        proxy_connect_timeout.*@'"        proxy_connect_timeout                  ${PROXY_CONNECT_TIMEOUT};"'@' /etc/nginx/nginx.conf
+fi
+
+if [ -n "$PROXY_SEND_TIMEOUT" ]; then
+      sed -i 's@^        proxy_send_timeout.*@'"        proxy_send_timeout                  ${PROXY_SEND_TIMEOUT};"'@' /etc/nginx/nginx.conf
+fi
+
+if [ -n "$PROXY_READ_TIMEOUT" ]; then
+      sed -i 's@^        proxy_read_timeout.*@'"        proxy_read_timeout                  ${PROXY_READ_TIMEOUT};"'@' /etc/nginx/nginx.conf
+fi
+
+if [ -n "$SEND_TIMEOUT" ]; then
+      sed -i 's@^        send_timeout.*@'"        send_timeout                  ${SEND_TIMEOUT};"'@' /etc/nginx/nginx.conf
+fi
+
 
 if [ -n "$PROXY_PASS" ]; then
       sed -i 's@^            proxy_pass.*@'"            proxy_pass ${PROXY_PASS};"'@' /etc/nginx/nginx.conf
